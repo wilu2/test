fac5c607f440ea4389461ca722b79c81001592e5
WenYangguang <yangguang_wen@intsig.net>
2020-07-18 20:11:35 +0800

    update api

--- test/customers_test.lua
+++ test/customers_test.lua
@@ -1,7 +1,7 @@
 local cjson = require("cjson.safe")
 local httptest = require("resty.http_test")
 
-local ht = httptest.new({ unit_name = "customer", host = "127.0.0.1", port = 8080})
+local ht = httptest.new({ unit_name = "customer", host = "127.0.0.1", port = 8080 })
 
 local info1 = {
   name = "winn",
@@ -20,23 +20,23 @@ function ht:init()
    self:init_schema_from_openapi_url("/openapi.json")
 end
 
-function ht:test_001_customers_list()
-  self:get("/customers")
-  self:response_have_status(200)
-  self:response_validate_schema("getAllCustomer")
-end
-
-function ht:test_002_add_customer_post_invalid_body()
+function ht:test_001_add_customer_post_invalid_body()
   self:post("/customers")
   self:response_have_status(406)
 end
 
-function ht:test_003_add_customer()
+function ht:test_002_add_customer()
   self:post("/customers",{body = cjson.encode(info1)})
   self:response_have_status(200)
   self:response_validate_schema("addCustomer")
   local json = self:response_to_json()
   generated_id = json.id
+end
+
+function ht:test_003_customers_list()
+  self:get("/customers")
+  self:response_have_status(200)
+  self:response_validate_schema("getAllCustomer")
 end
 
 function ht:test_004_get_customer()
