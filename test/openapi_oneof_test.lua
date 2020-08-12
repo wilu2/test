7455bc0c44a2666b88b7c89fefd19d8834c3d984
WenYangguang <yangguang_wen@intsig.net>
2020-08-12 19:23:01 +0800

    feat: add test for oneOf

---
+++ test/openapi_oneof_test.lua
@@ -0,0 +1,89 @@
+require("http_test")
+
+local openapi = [=[
+openapi: 3.0.3
+info:
+  title: oneof
+  version: 0.0.1
+  
+paths:
+  /oneof_test:
+    get:
+      summary:  oneOf test
+      responses:
+        '200':
+          description: OK
+          content:
+            text/plain:
+              schema:
+                oneOf:
+                  - $ref: '#/components/schemas/rectangle'
+                  - $ref: '#/components/schemas/sector'
+components:
+  schemas:
+    rectangle:
+      type: array
+      items:
+        type: object
+        properties:
+          property:
+            type: string
+            enum: 
+              - width
+              - height
+          value:
+            type: integer
+    sector:
+      type: array
+      items:
+        type: object
+        properties:
+          property:
+            type: string
+            enum:
+              - radius
+              - angle
+          value:
+            type: integer
+]=]
+
+local rectangle = [=[
+[
+    { "property": "width", "value": 100 },
+    { "property": "height", "value": 80 }
+]
+]=]
+
+local sector = [=[
+[
+    { "property": "radius", "value": 100 },
+    { "property": "angle", "value": 80 }
+]
+]=]
+
+local invalid_shape = [=[
+[
+    { "property": "width", "value": 100 },
+    { "property": "angle", "value": 80 }
+]
+]=]
+
+local function res(url, method, status, content, body)
+    return {
+      url = url,
+      method = method,
+      res = { 
+        status = status, 
+        body = body,
+        headers = {
+          ["Content-Type"] = content,
+        }
+      }
+    }
+end
+
+test("oneof_test", function()
+  assert.is_validated_against_openapi(res("/oneof_test", "GET", 200, "text/plain", rectangle), openapi)
+  assert.is_validated_against_openapi(res("/oneof_test", "GET", 200, "text/plain", sector), openapi)
+  assert.are_not.is_validated_against_openapi(res("/oneof_test", "GET", 200, "text/plain", invalid_shape), openapi)
+end)
